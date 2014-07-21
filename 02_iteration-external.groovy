273021287a5fec67aae8a02845490f50a4c77458
Mario Garcia <mario.ggar@gmail.com>
2014-07-21 12:29:54 +0200

    Added exercise to iteration-external

--- 02_iteration-external.groovy
+++ 02_iteration-external.groovy
@@ -13,15 +13,15 @@ def ages = [22, 45, 64, 68, 47, 87, 45, 44, 45, 24, 37]
 
 def people = [cities, names, ages].combinations() as Person[]
 
- // _____      _                        _ _          _   _                              _          _ _ 
+ // _____      _                        _ _          _   _                              _          _ _
  //| ____|_  _| |_ ___ _ __ _ __   __ _| (_)______ _| |_(_) ___  _ __    ___ _   _  ___| | _____  | | |
  //|  _| \ \/ / __/ _ \ '__| '_ \ / _` | | |_  / _` | __| |/ _ \| '_ \  / __| | | |/ __| |/ / __| | | |
  //| |___ >  <| ||  __/ |  | | | | (_| | | |/ / (_| | |_| | (_) | | | | \__ \ |_| | (__|   <\__ \ |_|_|
  //|_____/_/\_\\__\___|_|  |_| |_|\__,_|_|_/___\__,_|\__|_|\___/|_| |_| |___/\__,_|\___|_|\_\___/ (_|_)
- //                                                                                                    
+ //
 
 class Messy {
-    
+
     List<Person> findAllPeopleOlderThan30(Person[] all) {
         List<Person> filteredPeople = []
         all.each { person ->
@@ -31,29 +31,30 @@ class Messy {
         }
         return filteredPeople
     }
-    
+
 }
 
 assert new Messy()
     .findAllPeopleOlderThan30(people)
     .size() == 1089
-    
-    
-//  _____    _ _____ ____   ____ ___ ____ ___ ___  ____   
-// | ____|  | | ____|  _ \ / ___|_ _/ ___|_ _/ _ \/ ___|  
-// |  _| _  | |  _| | |_) | |    | | |    | | | | \___ \  
-// | |__| |_| | |___|  _ <| |___ | | |___ | | |_| |___) | 
-// |_____\___/|_____|_| \_\\____|___\____|___\___/|____/  
-//            
+
+
+//  _____    _ _____ ____   ____ ___ ____ ___ ___  ____
+// | ____|  | | ____|  _ \ / ___|_ _/ ___|_ _/ _ \/ ___|
+// |  _| _  | |  _| | |_) | |    | | |    | | | | \___ \
+// | |__| |_| | |___|  _ <| |___ | | |___ | | |_| |___) |
+// |_____\___/|_____|_| \_\\____|___\____|___\___/|____/
+//
 
 // [1] ELIMINAR ITERACION EXTERNA
 // [2] REFACTORIZAR CONDICION A UN METODO
 // [3] UTILIZAR METODO COMO CLOSURE
-// [4] UTILIZAR GPARS 
+// [4] UTILIZAR GPARS
+// [5] GENERALIZAR A findAllPeopleOlderThan(Integer age, Person[] all)
 
 /*
     import static groovyx.gpars.GParsPool.withPool
-    
+
     return withPool {
         people.findAllParallel(condition)
     }
